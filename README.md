5c89a5e7d83d38945b07e4421334156cb442c2a6
Dayrons <dayronstovar@gmail.com>
2024-04-10 16:02:48 -0400

    Actualizacion de archivo readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# cli_html_to_doc
+# Herramienta CLI para generar reportes
+
+## Descripcion
+
+Crea reportes automatizados mediante plantillas jinja2 con solo algunos comandos
+
+## Instalacion
+
+Si trabajas mendiante entorno virtual luego de crearlo en el proyecto
+
+ejecuta el siguiente comando
+```bash
+pip install -r requirenment.txt
+```
+## Configuracion
+El siguiente comando te pedira la ruta donde estaran ubicadas tus plantillas y la ruta donde se estaran exportando los documentos
+
+```bash
+python src/script.py config
+```
+o puedes ejecutar el comando ya indicando ambos parametros
+
+```bash
+python src/script.py config --path_templates ruta/a/tus/plantillas --path_export ruta/a/tu/directorio_de_exportacion
+```
+
+
--- src/script.py
+++ src/script.py
@@ -11,45 +11,49 @@ import os
 def main():
     pass
 
-
 @main.command()
-@click.option('--file_input', prompt='Nombre del template', help='Nombre del template con el que se generara el reporte(template.html)')
+@click.option('--template', prompt='Nombre del template', help='Nombre del template con el que se generara el reporte(template.html)')
 @click.option('--file_output', prompt='Nombre del salida', help='Nombre de exportacion del archivo(reporte.pdf)')
 @click.option('--variables', default="{}", prompt='Variables para la plantilla', help='Variables para la plantilla en formato JSON.')
-def generate_report(file_input, file_output, variables):
+def generate_report(template, file_output, variables):
     
     if not Config.select().count():
-        click.echo("No has configurado la ruta de las plantillas y la ruta de exportación. Ejecuta el comando config para configurarlas.")
+        click.echo(click.style("No has configurado la ruta de las plantillas y la ruta de exportación. Ejecuta el comando config para configurarlas.", fg='red'))
         return
 
-    for config in configs:
-        try:
-
-            template_path = os.path.join(config.path_templates, file_input)
-            
+    try:
+            config = Config.get(Config.id==1)
             export_path = os.path.join(config.path_export, file_output)
             
-            env = Environment(loader=FileSystemLoader('/'))
-            template = env.get_template(template_path)
+            env = Environment(loader=FileSystemLoader(config.path_templates))
+            template = env.get_template(template)
 
-            
+
             variables_dict = json.loads(variables)
             html = template.render(variables_dict)
 
             
             pdfkit.from_string(html, export_path)
-            click.echo(f"Archivo PDF generado con éxito: {export_path}")
+            click.echo(click.style(f"Archivo PDF generado con éxito: {export_path}", fg='green'))
             
-        except Exception as e:
-            click.echo(f"Error al generar el archivo PDF: {str(e)}")
-            
-
-
+    except Exception as e:
+        click.echo(click.style(f"Error al generar el archivo PDF: {str(e)}", fg='red'))
+        
 @main.command()
-@click.option('--template_path', prompt='Ruta de la plantilla', help='Ruta de la plantilla HTML.',callback=prompt_if_config)
-@click.option('--pdf_output', prompt='Salida del PDF', help='Ruta del archivo PDF de salida.',callback=prompt_if_config)
-def config():
-    pass      
+@click.option('--path_templates', prompt='Ruta de la plantillas', help='Ruta de las plantillas html para los reportes')
+@click.option('--path_export', prompt='Ruta de exportacion', help='Ruta del directorio donde se exportaran los PDF')
+def config(path_templates, path_export):
+    if  Config.select().count() >0:
+        config = Config.get(Config.id==1)
+        config.path_templates = path_templates
+        config.path_export = path_export
+        config.save()
+        click.echo(click.style("Configuración actualizada con éxito.", fg='green'))
+        return
+    
+    
+    Config.create(path_templates=path_templates, path_export=path_export)
+    click.echo(click.style("Configuración guardada con éxito.", fg='green'))    
 
 if __name__ == '__main__':
     if not Config.table_exists():
